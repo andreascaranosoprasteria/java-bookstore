20bcd3c50030bbbe08e24c28e5c7cb3bba5bf40e
SCARANO Andrea <andrea.scarano@soprasteria.com>
2022-10-04 16:48:35 +0200

    Prima Commit Progetto

---
+++ src/com/soprasteria/Libro.java
@@ -0,0 +1,34 @@
+package com.soprasteria;
+
+import java.util.Scanner;
+
+public class Libro {
+	
+	static Scanner sc = new Scanner(System.in);		//Oggetto della classe Scanner per acquisizione da tastiera.
+	public static void main(String[] args) {
+		String isbn;						//contiene l'identificativo univoco
+		String title;						//titolo del libro
+		String author;						//autore del libro
+		int availability;					//disponibilità di copie del libro
+		boolean isAvailable;				//il libro è disponibile?
+		float price;						//prezzo del libro
+		
+		//acquisizione dei dati
+		System.out.print("Inserisci ISBN del libro: -->");
+		isbn = sc.nextLine();
+		System.out.print("\n Inserisci il titolo: -->");
+		title = sc.nextLine();
+		System.out.print("\n Inserisci l'autore: -->");
+		author = sc.nextLine();
+		System.out.print("\n Inserisci disponibilità: -->");
+		availability = sc.nextInt();
+		
+		if(availability > 0) isAvailable = true;
+		else isAvailable = false;
+		
+		if(isAvailable == true)System.out.println("/n/n>>Il prodotto è disponibile!");
+		else System.out.println("\n\n>>Il prodotto non è disponibile!<<");
+		
+	}
+
+}
